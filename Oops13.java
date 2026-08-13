5081c03fd19e164b8a341cbf90df76f212d58093
surendra <nynalasurendranaidu.25.csm@anits.edu.in>
2026-08-13 15:31:10 +0530

    java programming file34

---
+++ Oops13.java
@@ -0,0 +1,18 @@
+interface I{
+	void m();
+	int a=10;
+}
+class B implements I{
+	void m(){
+		System.out.println("Hello");
+	}
+}
+class Oops13{
+	public static void main(String[] args){
+		I i new B(0;
+		System.out.println(I.a);
+		System.out.println(i.a);
+		i.m();
+		System.out.println(B.a);
+	}
+}
